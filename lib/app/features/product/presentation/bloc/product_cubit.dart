985042744fc02257bc6f948eba7153994aaaf465
Shox4ux <shoxruxu70@gmail.com>
2025-12-26 21:51:29 -0800

    init

--- lib/app/features/product/presentation/bloc/product_cubit.dart
+++ lib/app/features/product/presentation/bloc/product_cubit.dart
@@ -1,22 +1,90 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:bloc/bloc.dart';
 import 'package:crm_app/app/features/common/data/repo/data_state.dart';
 import 'package:crm_app/app/features/product/data/model/product_write.dart';
-import 'package:meta/meta.dart';
-
-import 'package:crm_app/app/features/product/domain/usecase/create_prod_usecase.dart';
+import 'package:crm_app/app/features/product/domain/entity/product_entity.dart';
+import 'package:crm_app/app/features/product/domain/repo/product_repo_impl.dart';
+import 'package:crm_app/app/features/product_expense/data/model/expense_bulk_update.dart';
+import 'package:crm_app/app/features/product_expense/data/model/expense_create.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'product_state.dart';
 
 class ProductCubit extends Cubit<ProductState> {
-  CreateProdUsecase createProdUsecase;
+  final ProductRepo _repo;
+  late List<ProductEntity> _filtered;
 
-  ProductCubit(this.createProdUsecase) : super(ProductInitial());
+  ProductCubit(this._repo) : super(ProductState(changes: ProductChanges())) {
+    _filtered = [];
+    getAllProduct();
+  }
 
-  void createProd(ProductWrite body) async {
-    var res = await createProdUsecase(body: body);
+  void neutralState() {
+    emit(state.copyWith(status: ProdStatus.init));
+  }
+
+  void setChanges(ProductChanges changes) {
+    var oldState = state;
+    var newState = oldState.copyWith(changes: changes);
+    emit(newState);
+  }
+
+  List<ProductEntity> getFiltList() => _filtered;
+
+  void filter(String? query) {
+    if (query != null && query.isNotEmpty) {
+      _filtered = state.list
+          .where((c) => c.name.toLowerCase().contains(query))
+          .toList();
+    } else {
+      _filtered.clear();
+      _filtered = state.list;
+    }
+  }
+
+  void getAllProduct() async {
+    emit(state.copyWith(status: ProdStatus.loading));
+    var res = await _repo.getAllProduct();
+    if (res is DataSuccess) {
+      _filtered = res.data ?? [];
+      emit(state.copyWith(status: ProdStatus.success, list: res.data));
+    } else {
+      emit(state.copyWith(status: ProdStatus.error, msg: res.errorMsg));
+    }
+  }
+
+  Future<void> updateProduct({required int id}) async {
+    emit(state.copyWith(status: ProdStatus.loading));
+    var body = ProductCreate.fromMap(state.changes.toMap());
+    var res = await _repo.updateProduct(id: id, body: body);
+    if (res is DataSuccess) {
+      if (!state.changes.isNotEmpty()) {
+        getAllProduct();
+      }
+    } else {
+      emit(state.copyWith(status: ProdStatus.error, msg: res.errorMsg));
+    }
+  }
+
+  void createProduct({
+    required ProductCreate body,
+    List<ProductExpenseCreate>? exps,
+  }) async {
+    emit(state.copyWith(status: ProdStatus.loading));
+    var res = await _repo.createProduct(body: body);
 
     if (res is DataSuccess) {
-    } else {}
+      _createExps(res.data!.id, exps);
+    } else {
+      emit(state.copyWith(status: ProdStatus.error, msg: res.errorMsg));
+    }
+  }
+
+  void deleteProduct({required int id}) async {
+    emit(state.copyWith(status: ProdStatus.loading));
+    var res = await _repo.deleteProduct(id: id);
+    if (res is DataSuccess) {
+      getAllProduct();
+    } else {
+      emit(state.copyWith(status: ProdStatus.error, msg: res.errorMsg));
+    }
   }
 }
